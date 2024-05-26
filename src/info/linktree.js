a168a8190cb5d6de136cac9c73eeec2deff20969
Dylan Alexander <dylanp.arango12@gmail.com>
2024-05-26 11:42:49 -0500

    Added: Link Tree

---
+++ src/info/linktree.js
@@ -0,0 +1,24 @@
+const TreeLinks = [
+    {
+      icon: 'bi bi-globe2',
+      title: 'Website',
+      url: '/'
+    },
+    {
+      icon: 'bi bi-tiktok',
+      title: 'Tik Tok',
+      url: '#'
+    },
+    {
+      icon: 'bi bi-instagram',
+      title: 'Instagram',
+      url: 'https://instagram.com/rise.jna'
+    },
+    {
+      icon: 'bi bi-youtube',
+      title: 'Youtube',
+      url: '#'
+    }
+  ];
+
+export default TreeLinks
